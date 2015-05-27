0617c3bf6410a47bf239fa6947c22849819d3311
smallY <smallyinglife@163.com>
2015-05-27 16:33:48 +0800

    Initial Commit

---
+++ VSTSTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSTSTool
+//
+//  Created by mac on 5/27/15.
+//  Copyright (c) 2015 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSTSTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSTSTool
+//
+//  Created by mac on 5/27/15.
+//  Copyright (c) 2015 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
